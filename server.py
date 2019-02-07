55d5265fd90b0b8d74102f030b35a2e156049043
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-06 17:53:10 -0800

    log out and log in routes

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from jinja2 import StrictUndefined
 
 from flask import (Flask, render_template, redirect, request, flash,
                    session)
+from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Rating, Movie, connect_to_db, db
@@ -23,7 +24,12 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-
+    # if session['user_id']:
+    #     print(session['user_id'])
+    #     user_session = session.user_id
+    # else:
+    #     user_session = None
+    
     return render_template("homepage.html")
 
 @app.route('/users')
@@ -44,25 +50,32 @@ def register_process():
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if email in User.email:
-        return "Logged in"
-    else:
-        Query = """
-        INSERT INTO users(email, password)
-        VALUES (:email, :password)
-        """
-        db.session.execute(
-            Query, {
-                "email": email,
-                "password": password
-            }
-        )
+    # finding id to log user in because we do not have
+    #emails
+    user = db.session.query(User.user_id).filter_by(email=email).first()
+    print('USER:', user)
 
+    if user == None:
+
+        new_user = User(email=email, password=password)
+
+        db.session.add(new_user)
         db.session.commit()
-
+        flash('You\'ve been added!')
+        return redirect("/") 
+    else:
+        session['user_id'] = user
+        print('\n\n\n\n\n')
+        print('HI')    
+        flash('You are logged in!')
         return redirect("/") 
 
+@app.route('/logout')
+def logout():
 
+    del session['user_id']
+
+    return redirect('/')
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
